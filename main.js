0ee4d71927559547ecc2b99931b933e618022b1b
Abdullah3002 <zubaerislam703@gmail.com>
2024-05-08 16:41:32 +0600

    second

--- main.js
+++ main.js
@@ -1,16 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menu-icon");
-    const navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector('#menu-icon'); // Changed from newIcon to menuIcon
+let navbar = document.querySelector('.navbar');
 
-    menuIcon.addEventListener("click", function() {
-        navbar.classList.toggle("menu-active");
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+
+//           scroll section active link
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+
+            });
+        };
     });
 
-    // Close the menu when a navigation link is clicked
-    const navLinks = document.querySelectorAll(".navbar a");
-    navLinks.forEach(link => {
-        link.addEventListener("click", function() {
-            navbar.classList.remove("menu-active");
-        });
-    });
+    //         sticky navbar 
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY >100);
+
+    //    romove toggle icon and navbar
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+}
+
+//  scroll reveal
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+})
+
+ScrollReveal(),reveal('.home-content, heading', {origin: 'top'});
+SceollReveal().reveal('.home-img, .services-container,portfolio-box, .contact form',{origin:'button'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+
+// typed js
+const typed = new Typed('multiple-text', {
+    strings : ['Frontend Developer', 'Web Design', 'Youtuber'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+
 });
